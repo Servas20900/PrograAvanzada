adcf4f80edb7d39b9370a107e14815ed2bc096db
Sebas Mendez <sebasmendeza09@gmail.com>
2025-10-18 14:44:53 -0600

    Controller vista log in, implementacion de razor en vista de log-in (Index) haciendo referencia al modelo Usuario

--- WebClases/WebClases/Controllers/HomeController.cs
+++ WebClases/WebClases/Controllers/HomeController.cs
@@ -1,21 +1,57 @@
 ﻿using System.Web.Mvc;
+using WebClases.Models;
 
+//get devuelve solo la vista, los post reciben datos del formulario y los procesan, por lo que dentro del controldor lleva una logica.
+// los post van sobre botones que tiene funcionalidades, para enviar datos, guardar, etc.
 namespace WebClases.Controllers
 {
     public class HomeController : Controller
     {
+        #region IniciarSession
+        [HttpGet]
         public ActionResult Index()
         {
             return View();
         }
+        [HttpPost]
+        public ActionResult Index(Usuario usuario)
+        {
+            return RedirectToAction("Principal","Home");
+        }
+        #endregion
+
+        #region Registro
+
+        [HttpGet]
         public ActionResult Registro()
         {
             return View();
         }
+
+        [HttpPost]
+        public ActionResult Registro(Usuario usuario)
+        {
+            return View();
+        }
+
+        #endregion
+
+        #region RecuperarAcceso
+        [HttpGet]
         public ActionResult RecuperarAcceso()
         { 
             return View();
         }
+
+        [HttpPost]
+        public ActionResult RecuperarAcceso(Usuario usuario)
+        {
+            return View();
+        }
+
+        #endregion
+
+        [HttpGet]
         public ActionResult Principal()
         {
             return View();
---
+++ WebClases/WebClases/Models/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebClases.Models
+{
+    public class Usuario
+    {
+        public String ID { get; set; }
+        public String Name { get; set; }
+        public String Email { get; set; }
+        public String Password { get; set; }
+
+    }
+}
